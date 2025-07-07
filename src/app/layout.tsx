5234c982a635aa356d104807c9f962c498264703
maximiloi <maximiloi@gmail.com>
2025-07-07 14:34:55 +0300

    fix: header + footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
-import type { Metadata } from 'next';
-import { Open_Sans } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import "./globals.css";
 
 const openSans = Open_Sans({
-  variable: '--font-open-sans',
-  subsets: ['latin', 'cyrillic'],
+  variable: "--font-open-sans",
+  subsets: ["latin", "cyrillic"],
 });
 
 export const metadata: Metadata = {
-  title: 'Автошкола | ООО «Компания АВТО ОНЛАЙН»',
+  title: "Автошкола | ООО «Компания АВТО ОНЛАЙН»",
   description:
-    'Автошкола ООО «Компания АВТО ОНЛАЙН» предлагает обучение на категорию B. Запишитесь на занятия онлайн.',
+    "Автошкола ООО «Компания АВТО ОНЛАЙН» предлагает обучение на категорию B. Запишитесь на занятия онлайн.",
 };
 
 export default function RootLayout({
@@ -19,8 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ru'>
-      <body className={`${openSans.variable} antialiased`}>{children}</body>
+    <html lang="ru">
+      <body className={`${openSans.variable} antialiased`}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
