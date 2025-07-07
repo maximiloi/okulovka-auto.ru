f11e47b4a24d39080ce04d3add2e3e9dc997a5c5
maximiloi <maximiloi@gmail.com>
2025-07-07 17:38:47 +0300

    feat: add document pages

---
+++ src/app/document/act-sanitary-epidemiologic-examination/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const PdfViewer = dynamic(() => import("../../component/ui/pdf-viewer"), {
+  ssr: false,
+});
+
+function Page() {
+  return (
+    <section className="container mx-auto flex flex-col gap-8 px-4 py-8">
+      <h2 className="text-3xl font-bold">
+        Акт санитарно-эпидемиологического обследования
+      </h2>
+
+      <PdfViewer src="/docs/act-sanitary-epidemiologic-examination.pdf" />
+    </section>
+  );
+}
+
+export default Page;
---
+++ src/app/document/conclusion-conformity-umb/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const PdfViewer = dynamic(() => import("../../component/ui/pdf-viewer"), {
+  ssr: false,
+});
+
+function Page() {
+  return (
+    <section className="container mx-auto flex flex-col gap-8 px-4 py-8">
+      <h2 className="text-3xl font-bold">
+        Заключение о соответствии учебно-материальной базы
+      </h2>
+
+      <PdfViewer src="/docs/conclusion-conformity-umb.pdf" />
+    </section>
+  );
+}
+
+export default Page;
