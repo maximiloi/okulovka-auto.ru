9eb88b6dda16e1b26842e107e5405948e8ca0f89
maximiloi <maximiloi@gmail.com>
2025-07-10 14:49:41 +0300

    add local dev address

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createMDX from "@next/mdx";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  allowedDevOrigins: ["10.0.1.9", "localhost"],
 };
 
 const withMDX = createMDX({
