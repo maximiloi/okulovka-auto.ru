64e8a40b72bf1fcb736dd6d5ee682918102dbc6c
maximiloi <maximiloi@gmail.com>
2025-07-16 14:38:17 +0300

    feat: add lottie anim 404 page

---
+++ src/app/component/CarTravel.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+const CarTravel = () => {
+  return <DotLottieReact src="/404/car_travel.lottie" loop autoplay />;
+};
+
+export default CarTravel;
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,21 @@
 import { Home } from "lucide-react";
 import Link from "next/link";
+import CarTravel from "./component/CarTravel";
 
 export default function NotFound() {
   return (
-    <section className="container mx-auto grid gap-8 px-4 py-8">
-      <h2 className="text-3xl font-bold">Страница не найдена</h2>
-      <Link
-        href="/"
-        className="hover:text-primary flex items-center gap-4 text-lg text-gray-800 transition-colors duration-300"
-      >
-        <Home /> На главную
-      </Link>
+    <section className="container mx-auto grid gap-8 px-4 py-8 md:grid-cols-2">
+      <div className="flex flex-col gap-8">
+        <h2 className="text-3xl font-bold">Страница не найдена</h2>
+        <Link
+          href="/"
+          className="hover:text-primary flex items-center gap-4 text-lg text-gray-800 transition-colors duration-300"
+        >
+          <Home /> На главную
+        </Link>
+      </div>
+
+      <CarTravel />
     </section>
   );
 }
